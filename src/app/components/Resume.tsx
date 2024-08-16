702ce50b435e27c005a30b20dc55f50ebe90718a
MattGeoSnyder <mattgeosnyder@gmail.com>
2024-08-16 14:21:27 -0400

    fix: Added updated Resume.

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -3,27 +3,33 @@ import { IconWrapper } from "@/app/components/icons/IconWrapper";
 import ResumeSvg from "@/assets/icons/Resume.svg";
 
 interface ResumeProps extends React.ComponentPropsWithoutRef<"div"> {
+	resumeUrl: string;
 }
 
-export function Resume({ ...props }: ResumeProps) {
+export function Resume({ resumeUrl, ...props }: ResumeProps) {
   function downloadFile(fileUrl: string) {
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = "Matthew_Snyder_Resume.pdf";
+    link.download = "MatthewSnyderResume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   }
 
   return (
-    <div {...props}>
-      <IconWrapper
-        text="Resume"
-        onClick={() => {
-          downloadFile("/Matthew_Snyder_Resume.pdf");
-        }}
-      >
-        <ResumeSvg className="max-w-full" fill="white" height="100%" width="100%" />
+    <div
+      {...props}
+      onClick={() => {
+        downloadFile(resumeUrl);
+      }}
+    >
+      <IconWrapper text="Resume">
+        <ResumeSvg
+          className="max-w-full"
+          fill="white"
+          height="100%"
+          width="100%"
+        />
       </IconWrapper>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Phone } from "@/app/components/Phone";
 import { Resume } from "@/app/components/Resume";
@@ -14,11 +14,18 @@ export default function Home() {
         id="home-page"
         className="w-full h-full md:p-28 grid md:grid-cols-3 md:grid-rows-2 md:gap-16 grid-cols-1 grid-flow-row auto-rows-fr max-sm:px-1 px-12"
       >
-        <Resume className="md:row-span-2 md:px-0" key={"Resume"}/>
-        <Phone phoneNumber="412-477-1776" key={"Phone"}/>
-        <Email url="mailto:mattgeosnyder@gmail.com" key={"Email"}/>
-        <GitHub url="https://github.com/MattGeoSnyder" key={"GitHub"}/>
-        <LinkedIn url="https://www.linkedin.com/in/mattgeosnyder" key={"LinkedIn"}/>
+        <Resume
+          className="md:row-span-2 md:px-0"
+          key={"Resume"}
+          resumeUrl="/MatthewSnyderResume.pdf"
+        />
+        <Phone phoneNumber="412-477-1776" key={"Phone"} />
+        <Email url="mailto:mattgeosnyder@gmail.com" key={"Email"} />
+        <GitHub url="https://github.com/MattGeoSnyder" key={"GitHub"} />
+        <LinkedIn
+          url="https://www.linkedin.com/in/mattgeosnyder"
+          key={"LinkedIn"}
+        />
       </div>
       <Toaster />
     </>
